a42f4daee0d14d6f848bbea930a7c7b62c24ab9b
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-12 18:12:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 var isDate = function (input) {
   //   write your code here
+	if (input instanceof Date) {
+    return true;
+  }
+
+  // Check if the input is a string and can be parsed into a valid date
+  if (typeof input === 'string') {
+    const dateObject = new Date(input);
+    return !isNaN(dateObject.getTime());
+  }
+
+  // Check if the input is a number (timestamp) and can be converted to a valid date
+  if (typeof input === 'number') {
+    const dateObject = new Date(input);
+    return !isNaN(dateObject.getTime());
+  }
+
+  // If none of the above conditions are met, return false
+  return false;
 };
 
 // Do not change the code below.
